f7b362a42b27fb0cd2c4051cd2ef37e97e7d2afb
TY37zhang <83420931+TY37zhang@users.noreply.github.com>
2025-04-27 13:56:10 -0400

    misc<vercel>: added speed-insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import AuthProvider from "@/components/providers/session-provider";
 import { Toaster } from "sonner";
 import { Footer } from "@/components/Footer";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
     subsets: ["latin"],
@@ -54,6 +55,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
                     </div>
                     <Toaster />
                     <Analytics />
+                    <SpeedInsights />
                 </AuthProvider>
             </body>
         </html>
