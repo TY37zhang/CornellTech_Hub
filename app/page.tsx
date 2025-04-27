53e8f98840c3a7c978cf2c58a8d01a9078666582
TY37zhang <83420931+TY37zhang@users.noreply.github.com>
2025-04-27 13:50:36 -0400

    misc<home>: update video

--- app/page.tsx
+++ app/page.tsx
@@ -27,16 +27,14 @@ export default function Dashboard() {
             <main className="flex-1">
                 <section className="w-full py-12 md:py-24 lg:py-32 bg-gradient-to-b from-red-50 to-white dark:from-red-950/20 dark:to-background">
                     <div className="container px-4 md:px-6">
-                        <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
+                        <div className="grid gap-6 lg:grid-cols-[1fr_500px] lg:gap-12 xl:grid-cols-[1fr_700px]">
                             <div className="flex flex-col justify-center space-y-4">
                                 <div className="space-y-2">
                                     <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
                                         Cornell Tech Hub
                                     </h1>
                                     <p className="max-w-[600px] text-muted-foreground md:text-xl">
-                                        Your one-stop platform for course
-                                        reviews, discussions, and campus
-                                        resources.
+                                        Your one-stop platform for Cornell Tech.
                                     </p>
                                 </div>
                                 {/* <div className="flex flex-col gap-2 min-[400px]:flex-row">
@@ -46,13 +44,16 @@ export default function Dashboard() {
                                     </Button>
                                 </div> */}
                             </div>
-                            <Image
-                                src="/images/home/campus_1.jpg"
-                                width={550}
-                                height={550}
-                                alt="Cornell Tech Campus"
-                                className="mx-auto aspect-video overflow-hidden rounded-xl object-cover sm:w-full lg:order-last"
-                            />
+                            <div className="mx-auto aspect-video overflow-hidden rounded-xl sm:w-full lg:order-last">
+                                <video
+                                    src="/videos/We%20are%20Cornell%20Tech.mp4"
+                                    autoPlay
+                                    muted
+                                    loop
+                                    playsInline
+                                    className="w-full h-full object-cover"
+                                />
+                            </div>
                         </div>
                     </div>
                 </section>
