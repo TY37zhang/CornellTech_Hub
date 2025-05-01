5afb6944878443aa7f7b9d1b9b47690270e78d85
TY37zhang <83420931+TY37zhang@users.noreply.github.com>
2025-05-01 16:19:36 -0400

    update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -16,7 +16,6 @@ module.exports = {
                 ],
             },
         ],
-        additionalSitemaps: ["https://www.cornelltechhub.info/sitemap.xml"],
     },
     exclude: ["/auth/*", "/api/*", "/settings", "/_next/*"],
     changefreq: "daily",
